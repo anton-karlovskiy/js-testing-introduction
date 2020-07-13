6120de63bd40d2c9fc3e825eb989f14dbf4425f8
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-07-13 13:33:57 +0300

    [feat] E2E test done

--- util.test.js
+++ util.test.js
@@ -1,4 +1,6 @@
 
+const puppeteer = require('puppeteer');
+
 const { generateText, checkAndGenerate } = require('./util');
 
 // MEMO: Unit Test
@@ -23,3 +25,22 @@ test('should generate a valid text output', () => {
   const text = checkAndGenerate('Max', 29);
   expect(text).toBe('Max (29 years old)');
 });
+
+test('should create an element with text and correct class', async () => {
+  const browser = await puppeteer.launch({
+    headless: true
+    // headless: false, // MEMO: run a browser with a UI
+    // slowMo: 80, // MEMO: slow down the operation so that we have a chance of seeing what's happening
+    // args: ['--window-size=1920,1080'] // MEMO: should lanuch a browser with this size
+  });
+  const page = await browser.newPage('');
+  await page.goto('file:///F:/07_development/js-testing-introduction/index.html');
+  // MEMO: you can find all commands in the official puppeteer docs
+  await page.click('input#name');
+  await page.type('input#name', 'Anna');
+  await page.click('input#age');
+  await page.type('input#age', '28');
+  await page.click('#btnAddUser');
+  const finalText = await page.$eval('.user-item', el => el.textContent);
+  expect(finalText).toBe('Anna (28 years old)');
+}, 10000);
